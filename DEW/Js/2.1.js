976c2f9c14963c96b1d52ea86a6672d73c378f77
EstebanAaron <lr96aron@gmail.com>
2024-09-19 18:23:41 +0100

    1

---
+++ DEW/Js/2.1.js
@@ -0,0 +1,15 @@
+function inverso() {
+const numero = parseInt(document.getElementById('numero').value);
+const parrafo = document.getElementById('Prueba');
+let cadena = numero.toString();
+let aux='';
+parrafo.innerHTML='';
+
+for (let i = 0; i < cadena.length; i++) {
+    aux+=cadena.charAt(cadena.length-(i+1));
+    
+}
+ alert(aux);
+ parrafo.innerHTML=aux;   
+}
+inverso();
