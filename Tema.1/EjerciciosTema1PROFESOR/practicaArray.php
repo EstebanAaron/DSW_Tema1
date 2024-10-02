266ea130607ff500dc082abb0bbd58de044470a2
EstebanAaron <lr96aron@gmail.com>
2024-10-02 17:50:53 +0100

    3

--- Tema.1/EjerciciosTema1PROFESOR/practicaArray.php
+++ Tema.1/EjerciciosTema1PROFESOR/practicaArray.php
@@ -28,6 +28,82 @@ $apple = array_splice($fruits, $pos, 1)[0] ;
 print_r($fruits);
 echo $apple;
 
+//1 creas array
+$fruits = ['manzana','platano','kiwi'];
+//2 muestras array
+print("1 y 2");
+print("<p>");
+print_r($fruits);
+print("</p>");
+//3 muestras longitud del array
+print("3");
+$cantidad = count($fruits);
+print("<p>Cantidad de elementos: $cantidad</p>");
+//4 añades al final del array
+print("4");
+array_push($fruits, 'naranja');
+print("<p>");
+print_r($fruits);
+print("</p>");
+//5 creas copia de Array
+print("5 y 6");
+$fruitsCopy = $fruits;
+//6 muestras copia de Array
+array_unshift($fruitsCopy, 'sandia');
+print("<p>");
+print_r($fruitsCopy);
+print("</p>");
+//7 mostramos q no se modifica el array de frutas original
+print("7");
+print("<p>");
+print_r($fruits);
+print("</p>");
+//8 busca la fruta papaya
+print("8");
+print("<p>");
+$result = array_search('papaya', $fruits);
+if($result == false){
+    print("No esta en el array de frutas");
+}else{
+    print("Esta en el array de frutas");
+}
+print("</p>");
+//9 añadimos la fruta papaya
+print("9");
+array_push($fruits, 'papaya');
+print("<p>");
+$result = array_search('papaya', $fruits);
+if($result == false){
+    print("No esta en el array de frutas");
+}else{
+    print("Esta en el array de frutas");
+}
+print("</p>");
+//10 buscamos la posicion de manzana
+print("10");
+print("<p>");
+$result = array_search('manzana', $fruits);
+if($result === false){
+    print("No esta en el array de frutas");
+}else{
+    print("Su posicion es: ". array_search('manzana', $fruits));
+}
+print("</p>");
+//11 Extrae manzana del array
+print("11");
+print("<p>");
+$result = array_search('manzana', $fruits);
+if($result === false){
+    print("No esta en el array de frutas");
+}else{
+    //esto elimina el dato array
+    unset($fruits[array_search('manzana', $fruits)]);
+    //y esto reindexa el array para que esten bien los indices
+    $fruits = array_values($fruits);
+    print_r($fruits);  
+}
+
+
 //12 .Inserta manzana en la posición 2.
 array_splice($fruits, 1, 0, $apple);
 print_r($fruits);
